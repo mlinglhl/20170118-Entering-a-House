8a52b046ffbf0b7af7b9df5bca4d509ada7beaf0
mlinglhl <minhung.ling@gmail.com>
2017-01-18 14:37:55 -0500

    Initial Commit

---
+++ 20170118 Entering a House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20170118 Entering a House
+//
+//  Created by Minhung Ling on 2017-01-18.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
